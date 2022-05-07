919b1ad67178f1d0a3afc983a4cd5aeea73af6ca
Mayon Francis <mayonfrancis@gmail.com>
2022-05-08 01:06:35 +0530

    Sample Trace

---
+++ scripts/testTransaction.js
@@ -0,0 +1,52 @@
+const hre = require("hardhat");
+const {Tracer} = require("./tracer");
+
+async function main() {
+
+  const [sender, receiver] = await ethers.getSigners();
+
+  console.log("Before");
+  console.log(ethers.utils.formatEther(await sender.getBalance()));
+  console.log(ethers.utils.formatEther(await receiver.getBalance()));
+  const tx = await sender.sendTransaction({
+    to: receiver.address,
+    value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+  });
+
+  console.log("EtherTransaction", tx);
+
+  console.log("After");
+  console.log(ethers.utils.formatEther(await sender.getBalance()));
+  console.log(ethers.utils.formatEther(await receiver.getBalance()));
+
+  // console.log(Tracer);
+  const tracer = new Tracer('http://localhost:8545');
+  // Generates Empty trace ???
+  await tracer.process(tx);
+  console.log("Ether Transfer",tracer.parsedTrace);
+// ******************************************************************
+
+
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  await greeter.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+  const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  // wait until the transaction is mined
+  await setGreetingTx.wait();
+
+  console.log("setGreetingTransaction", setGreetingTx);
+  await tracer.process(setGreetingTx);
+  console.log("setGreetingTx Trace",tracer.parsedTrace);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/tracer.js
@@ -0,0 +1,152 @@
+const ethers = require('ethers');
+
+let getProvider = function(url) {
+    return hre.ethers.provider;
+    // const rpcServer = new URL(url);
+    // var urlInfo;
+    // var provider = ethers.providers.WebSocketProvider;
+
+    // if (rpcServer.username != '' && rpcServer.password != '') {
+    //     urlInfo = {
+    //         url: `${rpcServer.origin}${rpcServer.pathName ? rpcServer.pathName : ''}`,
+    //         user: rpcServer.username,
+    //         password: rpcServer.password
+    //     };
+    // }
+    // else {
+    //     urlInfo = rpcServer.href;
+    // }
+
+    // if (rpcServer.protocol == 'http:' || rpcServer.protocol == 'https:') {
+    //     provider = ethers.providers.JsonRpcProvider;
+    // }
+    // else if (rpcServer.protocol == 'ws:' || rpcServer.protocol == 'wss:') {
+    //     provider = ethers.providers.WebSocketProvider;
+    // }
+
+    // return new provider(urlInfo);
+};
+
+const parseTrace = async (from, trace, provider) => {
+    const opCodes = ['CALL', 'CALLCODE', 'DELEGATECALL', 'STATICCALL', 'CREATE', 'CREATE2'];
+    const filteredData = trace.structLogs.filter(log => opCodes.indexOf(log.op) > -1 || log.pc == 1507);
+    const parsedOps = [];
+    for (const log of filteredData) {
+        try { 
+            switch(log.op) {
+                case 'CALL':
+                case 'CALLCODE':
+                    const inputStart = parseInt(log.stack[log.stack.length - 4], 16) * 2;
+                    const inputSize = parseInt(log.stack[log.stack.length - 5], 16) * 2;
+                    const input = `0x${log.memory.join('').slice(inputStart, inputStart + inputSize)}`;
+                    
+                    const deeperLogs = trace.structLogs.filter(returnLog => returnLog.pc == log.pc + 1);
+                    const outLog = trace.structLogs[trace.structLogs.indexOf(deeperLogs[0]) - 1];
+
+                    const outLogMemory = Buffer.from(outLog.memory.join(''));
+                    const outStart = parseInt(outLog.stack[outLog.stack.length - 1], 16) * 2;
+                    const outSize = parseInt(outLog.stack[outLog.stack.length - 2], 16) * 2;
+                    const out = `0x${outLogMemory.slice(outStart, outStart + outSize)}`;
+
+                    const address = `0x${log.stack[log.stack.length - 2].slice(-40)}`.toLowerCase();
+                    const bytecode = await provider.getCode(address);
+                    parsedOps.push({
+                        op: log.op,
+                        address: address,
+                        input: input,
+                        returnData: out != '0x' ? out : '',
+                        depth: log.depth,
+                        contractHashedBytecode: ethers.utils.keccak256(bytecode)
+                    })
+                    break;
+                case 'DELEGATECALL':
+                case 'STATICCALL': {
+                    const inputStart = parseInt(log.stack[log.stack.length - 3], 16) * 2;
+                    const inputSize = parseInt(log.stack[log.stack.length - 4], 16) * 2;
+                    const input = `0x${log.memory.join('').slice(inputStart, inputStart + inputSize)}`;
+                    
+                    const deeperLogs = trace.structLogs.filter(returnLog => returnLog.pc == log.pc + 1);
+                    const outLog = trace.structLogs[trace.structLogs.indexOf(deeperLogs[0]) - 1];
+                    const outLogMemory = Buffer.from(outLog.memory.join(''));
+                    const outStart = parseInt(outLog.stack[outLog.stack.length - 1], 16) * 2;
+                    const outSize = parseInt(outLog.stack[outLog.stack.length - 2], 16) * 2;
+                    const out = `0x${outLogMemory.slice(outStart, outStart + outSize)}`;
+
+                    const address = `0x${log.stack[log.stack.length - 2].slice(-40)}`.toLowerCase();
+                    const bytecode = await provider.getCode(address);
+                    parsedOps.push({
+                        op: log.op,
+                        address: address,
+                        input: input,
+                        returnData: out != '0x' ? out : '',
+                        depth: log.depth,
+                        contractHashedBytecode: ethers.utils.keccak256(bytecode)
+                    })
+                    break;
+                }
+                case 'CREATE':
+                case 'CREATE2': {
+                    const stackCopy = [...log.stack];
+                    stackCopy.pop();
+                    const p = parseInt(stackCopy.pop().valueOf(), 16) * 2;
+                    const n = parseInt(stackCopy.pop().valueOf(), 16) * 2;
+                    const s = `0x${stackCopy.pop()}`;
+
+                    const creationBytecode = `0x${log.memory.join('').slice(p, p + n)}`;
+                    const hashedCreationBytecode = ethers.utils.keccak256(creationBytecode);
+
+                    const address = ethers.utils.getCreate2Address(from, s, hashedCreationBytecode);
+
+                    const runtimeBytecode = await provider.getCode(address);
+                    const contractHashedBytecode = ethers.utils.keccak256(runtimeBytecode);
+
+                    parsedOps.push({
+                        op: log.op,
+                        address: address,
+                        depth: log.depth,
+                        contractHashedBytecode: contractHashedBytecode
+                    });
+                    break;
+                }
+                default:
+            }
+        } catch(error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    return parsedOps
+};
+
+class Tracer {
+    constructor(server) {
+        if (!server) throw '[Tracer] Missing parameter';
+        this.provider = getProvider(server);
+    }
+
+    async process(transaction) {
+        try {
+            this.transaction = transaction;
+            const rawTrace = await this.provider.send('debug_traceTransaction', [transaction.hash, {}]);
+            this.parsedTrace = await parseTrace(transaction.from, rawTrace, this.provider);
+        } catch(error) {
+            if (error.error && error.error.code == '-32601')
+                throw 'debug_traceTransaction is not available';
+            else
+                throw error;
+        }
+    }
+
+    async saveTrace(userId, workspace) {
+        try {
+            await processTrace(userId, workspace, this.transaction.hash, this.parsedTrace);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = {
+    Tracer: Tracer,
+}
